7a3c0d4d2594928b8f80e5be8abdeb3808d4ff79
Aharonyn <44743034+Aharonyn@users.noreply.github.com>
2019-08-02 11:32:03 +0300

    Update pi.py

--- SoftwareDevelopmentTask/pi.py
+++ SoftwareDevelopmentTask/pi.py
@@ -17,3 +17,14 @@ for i in range(num_of_logs): # iterating over all of the sentences
 
 output.write("=============================")
 output.close()
+
+### Time complexity: at the beginning, given that the number of logs is n, ordering them in a list, is O(n).  
+### The loop runs on all of the size 2 subsets of the list of logs, which means: O(n^2) iterations,
+### given that the number of logs is n. Given that the longest log length is m,
+### the time complexity for each iteration is O(m) for the replace procedure, O(m) for split and O(m) for the set procedure.
+### Given that t is the number of words in a log sentence, the set-minus operation takes O(t). Pop procedure is O(1).
+### To conclude: the runtime of this code is: O((n^2) * (m + t))
+
+### If I had two weeks for this assignment, I would choose to train a TF-IDF algorithm with data I already grouped, and help it recognize
+### the structures appearing in this kind of logs. The TF-IDF grades strings according to a given scale, using cosine functions,
+### and then finds the closest match to the given string. I Believe it would improve the time complexity inside each iteration.
