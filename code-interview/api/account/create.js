38ae49eded7689c9904713eaf9abf71afb0cf710
aharonyn <aharonyn@gmail.com>
2019-09-04 18:27:05 +0300

    Task A-1, improving /account/create so it only accepts POST requests

--- code-interview/api/account/create.js
+++ code-interview/api/account/create.js
@@ -1,8 +1,11 @@
 const Account = require('../../models/account/Account');
 
 module.exports = async function(req, res, next) {
-	const {email, name, age} = req.body;
-	const account = new Account({email, name, age});
-	await account.save();
-	return res.send({message: 'success'});
+	if (req.method === 'POST'){ // if the request method is POST, execute.
+		const {email, name, age} = req.body;
+		const account = new Account({email, name, age});
+		await account.save();
+		return res.send({message: 'success'});
+	}
+	return res.send({message: 'This is not a POST request'});
 };
