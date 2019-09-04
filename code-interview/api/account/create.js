f84b6f12d897b5b136e9fe2c0f95a55c929e06e2
aharonyn <aharonyn@gmail.com>
2019-09-04 18:30:07 +0300

    Task A-2, improving create to not allow a duplicate email address

--- code-interview/api/account/create.js
+++ code-interview/api/account/create.js
@@ -4,8 +4,13 @@ module.exports = async function(req, res, next) {
 	if (req.method === 'POST'){ // if the request method is POST, execute.
 		const {email, name, age} = req.body;
 		const account = new Account({email, name, age});
-		await account.save();
-		return res.send({message: 'success'});
+		const email_account = await Account.findOne({email: email}); // waiting for a response.
+		if (!email_account){ // if this email doesn't exist, saves to the DB. 
+			await account.save();
+			return res.send({message: 'success'});
+		} else { // if this email already exists.
+			return res.send({error: "email already exists"});
+		} 
 	}
 	return res.send({message: 'This is not a POST request'});
 };
